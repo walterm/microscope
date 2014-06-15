6356c710d15d666e71a060921085cef3b1c95e74
Walter Menendez <walterm@mit.edu>
2014-06-15 19:06:23 -0400

    Added posts collection

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,6 +1,7 @@
 Template.postItem.helpers({
   domain: function(){
     var a = document.createElement("a");
+    // This = each individual post item
     a.href = this.url;
     return a.hostname;
   }
